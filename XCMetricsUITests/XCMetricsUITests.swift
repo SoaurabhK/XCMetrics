beacff6cb0f546888490344f0dd0e80d98d8c355
Soaurabh Kakkar <soaurabh.kakkar@gmail.com>
2020-08-21 23:51:51 +0530

    Using private initWithProcessName: API to initialise XCTCPUMetric and XCTMemoryMetric in UITests.

--- XCMetricsUITests/XCMetricsUITests.swift
+++ XCMetricsUITests/XCMetricsUITests.swift
@@ -9,6 +9,9 @@ import XCTest
 
 final class XCMetricsUITests: XCTestCase {
     
+    typealias MetricAlloc = @convention(c) (XCTMetric.Type, Selector) -> NSObject
+    typealias MetricInitWithProcessName = @convention(c) (NSObject, Selector, String) -> XCTMetric
+    
     override func setUpWithError() throws {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
@@ -27,10 +30,38 @@ final class XCMetricsUITests: XCTestCase {
         let signpostMetric = self.signpostMetric(for: SignpostName.processImage)
         
         // NOTE: XCTClockMetric doesn't make much sense in UITest(s) because it will target the time of execution in current process, not the process targetted by XCUIApplication proxy.
-        // NOTE: XCTCPUMetric(application: app) and XCTMemoryMetric(application: app) won’t work on iOS 14. However, It will work when checked on iOS 13.6, I still doubt their accuracy, so we should avoid it.
-        measure(metrics: [signpostMetric, XCTCPUMetric(application: app), XCTStorageMetric(application: app), XCTMemoryMetric(application: app)], options: measureOptions) {
+        // NOTE: XCTCPUMetric(application: app) and XCTMemoryMetric(application: app) won’t work on iOS 14. However, It will work when checked on iOS 13.6, I still doubt their accuracy, so we should discard any outlier cpu/memory values.
+        let memoryMetric: XCTMetric
+        let cpuMetric: XCTMetric
+        if #available(iOS 14, *) {
+            memoryMetric = self.initWithProcessName(for: XCTMemoryMetric.self, processName: "XCMetrics")
+            cpuMetric = self.initWithProcessName(for: XCTCPUMetric.self, processName: "XCMetrics")
+        } else {
+            memoryMetric = XCTMemoryMetric(application: app)
+            cpuMetric = XCTCPUMetric(application: app)
+        }
+        
+        measure(metrics: [memoryMetric, cpuMetric, signpostMetric, XCTStorageMetric(application: app)], options: measureOptions) {
             processButton.tap()
         }
+    }
+    
+    //initWithProcessName: is a private API, used to ensure support for iOS 14.
+    private func initWithProcessName(for type: XCTMetric.Type, processName: String) -> XCTMetric {
+        guard type is XCTMemoryMetric.Type ||
+              type is XCTCPUMetric.Type ||
+              type is XCTStorageMetric.Type else {
+            fatalError("CPU, Storage and Memory metric implements initWithProcessName:")
+        }
+        let allocSelector = NSSelectorFromString("alloc")
+        let allocIMP = method_getImplementation(class_getClassMethod(type.self, allocSelector)!)
+        let allocMethod = unsafeBitCast(allocIMP, to: MetricAlloc.self)
+        let result = allocMethod(type.self, allocSelector)
+        
+        let initSelector = NSSelectorFromString("initWithProcessName:")
+        let methodIMP = result.method(for: initSelector)
+        let initMethod = unsafeBitCast(methodIMP, to: MetricInitWithProcessName.self)
+        return initMethod(result, initSelector, processName)
     }
     
     private func signpostMetric(for name: StaticString) -> OSSignpostMetric {
